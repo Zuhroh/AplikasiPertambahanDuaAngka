b6b8d2deb137a38f2541669c7c315c04d05d6a68
Siti Zuhrah <zuhrah.uniska@gmail.com>
2024-11-03 16:23:23 +0800

    Menambahkan code program

--- src/PertambahanDuaAngkaFrame.java
+++ src/PertambahanDuaAngkaFrame.java
@@ -1,17 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
+import java.awt.event.KeyEvent;
+import javax.swing.JOptionPane;
 
-/**
- *
- * @author ZUHROH
- */
 public class PertambahanDuaAngkaFrame extends javax.swing.JFrame {
 
-    /**
-     * Creates new form PertambahanDuaAngkaFrame
-     */
     public PertambahanDuaAngkaFrame() {
         initComponents();
         setSize(450, 300);
@@ -32,13 +23,13 @@ public class PertambahanDuaAngkaFrame extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
-        jTextField3 = new javax.swing.JTextField();
+        txtPertama = new javax.swing.JTextField();
+        txtKedua = new javax.swing.JTextField();
+        txtHasil = new javax.swing.JTextField();
         jPanel2 = new javax.swing.JPanel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
+        btnHitung = new javax.swing.JButton();
+        btnHapus = new javax.swing.JButton();
+        btnKeluar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Aplikasi Pertambahan Dua Angka");
@@ -82,48 +73,71 @@ public class PertambahanDuaAngkaFrame extends javax.swing.JFrame {
         gridBagConstraints.anchor = java.awt.GridBagConstraints.LINE_END;
         jPanel1.add(jLabel3, gridBagConstraints);
 
-        jTextField1.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
-        jTextField1.setText("jTextField1");
+        txtPertama.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
+        txtPertama.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txtPertamaKeyTyped(evt);
+            }
+        });
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         gridBagConstraints.ipadx = 20;
         gridBagConstraints.ipady = 20;
-        jPanel1.add(jTextField1, gridBagConstraints);
+        jPanel1.add(txtPertama, gridBagConstraints);
 
-        jTextField2.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
-        jTextField2.setText("jTextField2");
+        txtKedua.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
+        txtKedua.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txtKeduaKeyTyped(evt);
+            }
+        });
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 1;
         gridBagConstraints.gridy = 1;
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         gridBagConstraints.ipadx = 20;
         gridBagConstraints.ipady = 20;
-        jPanel1.add(jTextField2, gridBagConstraints);
+        jPanel1.add(txtKedua, gridBagConstraints);
 
-        jTextField3.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
-        jTextField3.setText("jTextField3");
+        txtHasil.setEditable(false);
+        txtHasil.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 1;
         gridBagConstraints.gridy = 3;
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         gridBagConstraints.ipadx = 20;
         gridBagConstraints.ipady = 20;
-        jPanel1.add(jTextField3, gridBagConstraints);
+        jPanel1.add(txtHasil, gridBagConstraints);
 
         jPanel2.setBackground(new java.awt.Color(0, 102, 102));
         jPanel2.setLayout(new java.awt.GridLayout(1, 0, 5, 0));
 
-        jButton1.setFont(new java.awt.Font("Century Gothic", 1, 12)); // NOI18N
-        jButton1.setText("Hitung");
-        jPanel2.add(jButton1);
+        btnHitung.setFont(new java.awt.Font("Century Gothic", 1, 12)); // NOI18N
+        btnHitung.setText("Hitung");
+        btnHitung.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnHitungActionPerformed(evt);
+            }
+        });
+        jPanel2.add(btnHitung);
 
-        jButton2.setFont(new java.awt.Font("Century Gothic", 1, 12)); // NOI18N
-        jButton2.setText("Hapus");
-        jPanel2.add(jButton2);
+        btnHapus.setFont(new java.awt.Font("Century Gothic", 1, 12)); // NOI18N
+        btnHapus.setText("Hapus");
+        btnHapus.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnHapusActionPerformed(evt);
+            }
+        });
+        jPanel2.add(btnHapus);
 
-        jButton3.setFont(new java.awt.Font("Century Gothic", 1, 12)); // NOI18N
-        jButton3.setText("Keluar");
-        jPanel2.add(jButton3);
+        btnKeluar.setFont(new java.awt.Font("Century Gothic", 1, 12)); // NOI18N
+        btnKeluar.setText("Keluar");
+        btnKeluar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnKeluarActionPerformed(evt);
+            }
+        });
+        jPanel2.add(btnKeluar);
 
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 1;
@@ -137,6 +151,44 @@ public class PertambahanDuaAngkaFrame extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnHitungActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnHitungActionPerformed
+        try {
+            int pertama = Integer.parseInt(txtPertama.getText());
+            int kedua = Integer.parseInt(txtKedua.getText());
+            int hasil = pertama + kedua;
+            txtHasil.setText("" + hasil);
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "GAGAL MENGHITUNG!");
+        }
+    }//GEN-LAST:event_btnHitungActionPerformed
+
+    private void txtPertamaKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtPertamaKeyTyped
+        char c = evt.getKeyChar();
+            if (!Character.isDigit(c) && c != KeyEvent.VK_BACK_SPACE) {
+                evt.consume();
+                JOptionPane.showMessageDialog(null, "Masukkan hanya ANGKA!");
+            }
+    }//GEN-LAST:event_txtPertamaKeyTyped
+
+    private void txtKeduaKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtKeduaKeyTyped
+        char c = evt.getKeyChar();
+            if (!Character.isDigit(c) && c != KeyEvent.VK_BACK_SPACE) {
+                evt.consume();
+                JOptionPane.showMessageDialog(null, "Masukkan hanya ANGKA!");
+            }
+    }//GEN-LAST:event_txtKeduaKeyTyped
+
+    private void btnHapusActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnHapusActionPerformed
+        txtPertama.setText("");
+        txtKedua.setText("");
+        txtHasil.setText("");
+        txtPertama.requestFocus();
+    }//GEN-LAST:event_btnHapusActionPerformed
+
+    private void btnKeluarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnKeluarActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_btnKeluarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -174,16 +226,16 @@ public class PertambahanDuaAngkaFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
+    private javax.swing.JButton btnHapus;
+    private javax.swing.JButton btnHitung;
+    private javax.swing.JButton btnKeluar;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
+    private javax.swing.JTextField txtHasil;
+    private javax.swing.JTextField txtKedua;
+    private javax.swing.JTextField txtPertama;
     // End of variables declaration//GEN-END:variables
 }
